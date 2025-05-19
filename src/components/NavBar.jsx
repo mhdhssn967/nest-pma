499f67cdccb50967a50499c4615eed2f9c2993f1
mhdhssn <mhdhssn967@gmail.com>
2025-05-19 17:40:02 +0530

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import logo from '../assets/OQ.png'
+import './navBar.css'
+import getCurrentUserID from '../services/getUserID'
+import getEmployeeDetails from '../services/getEmployeeDetails'
+import { auth } from '../firebaseConfig'
+
+
+const NavBar = ({employeeName, companyName}) => {
+
+  const [user, setUser]=useState('')
+
+  
+  const [empDetails, setEmpDetails]=useState([])
+  console.log(empDetails);
+  
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const currentUser = await getCurrentUserID();
+      const details = await getEmployeeDetails();
+  
+      setEmpDetails(details);
+        const matched = details.find((emp) => emp.userID === currentUser);
+      if (matched) {
+        setUser(matched.userName);
+      }
+  
+      if (!currentUser) {
+        navigate("/login"); // Redirect if not logged in
+      }
+    };
+  
+    fetchUser();
+  }, []);
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    window.location.href = '/login'; // Redirect to login
+  };
+  
+  return (
+    <>
+    <div className='nav'>
+        <div className='navLeft'><img src={logo} alt="" /><h3> Oqulix Product Management</h3> </div>
+        <h2 className='company-name'>{companyName}</h2>
+        <div style={{display:'flex',alignItems:'center',marginRight:'100px'}}>
+        <i className='fa-solid fa-circle-user' style={{fontSize:'30px',marginRight:'2%'}}></i><h3 style={{fontWeight:'500',marginBottom:'3px',fontSize:'25px', textWrap:'nowrap'}}>          
+           {employeeName}</h3> <button onClick={handleLogout} className='logout-btn'>Logout</button></div>
+        </div>
+        
+    </>
+  )
+}
+
+export default NavBar
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import TaskView from '../components/TaskView'
+import getCurrentUserID from '../services/getUserID'
+import { getCompanyDetails, ifAdmin } from '../services/getEmployeeDetails'
+import NavBar from '../components/NavBar'
+
+const HomePage = () => {
+  const [currentUser, setCurrentUser]=useState('')  
+  const [isAdmin, setIsAdmin]=useState(false)
+  const [companyId,setCompanyId]=useState()
+  const [employeeName, setEmployeeName]=useState()
+  const [companyName,setCompanyName]=useState()      
+  
+
+  useEffect(()=>{
+    const fetchUser=async()=>{
+      const userRef=await getCurrentUserID()
+      setCurrentUser(userRef)
+    };fetchUser()
+  },[])
+
+
+  // Get company details
+
+useEffect(() => {
+  const fetchDetails = async () => {
+    const companyData = await getCompanyDetails(currentUser);
+    setCompanyId(companyData.companyId)
+    setCompanyName(companyData.companyName)
+    setEmployeeName(companyData.employeeName)
+    
+  };
+
+  if (currentUser) fetchDetails();
+}, [currentUser]);
+  
+  useEffect(()=>{
+    const checkAdmin=async()=>{
+      await ifAdmin(currentUser)?setIsAdmin(true):setIsAdmin(false)
+    };checkAdmin()
+  },[currentUser])
+
+
+  return (
+    <>
+    <NavBar employeeName={employeeName} companyName={companyName}/>
+    <TaskView currentUser={currentUser} isAdmin={isAdmin} companyId={companyId}/>
+    </>
+  )
+}
+
+export default HomePage
